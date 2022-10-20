7d8a8094b07950456920639604545c559eb4c7cb
youbaidjis <youba.etud@gmail.com>
2022-10-20 14:56:02 -0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import App from "./App";
 import rootReducer from "./reducer";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composedEnhancer
 );
 
 ReactDOM.render(
